cd59ca6f7e06f5e7c22c435a8c4e2ed3fa06ea5d
putriwardhani <106011150+putriwardhani@users.noreply.github.com>
2022-05-22 17:02:28 +0800

    Create index.php

---
+++ index.php
@@ -0,0 +1,307 @@
+<?php
+include"koneksi.php";
+$data=mysqli_query($con,"select * from produk ");
+
+?>
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>Apple Rental Kupang</title>
+    <meta name="description" content="">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="robots" content="all,follow">
+    <!-- Bootstrap CSS-->
+    <link rel="stylesheet" href="vendor/bootstrap/css/bootstrap.min.css">
+    <!-- Font Awesome CSS-->
+    <link rel="stylesheet" href="vendor/font-awesome/css/font-awesome.min.css">
+    <!-- Google fonts - Roboto -->
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:100,300,400,700">
+    <!-- owl carousel-->
+    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.carousel.css">
+    <link rel="stylesheet" href="vendor/owl.carousel/assets/owl.theme.default.css">
+    <!-- theme stylesheet-->
+    <link rel="stylesheet" href="css/style.default.css" id="theme-stylesheet">
+    <!-- Custom stylesheet - for your changes-->
+    <link rel="stylesheet" href="css/custom.css">
+    <!-- Favicon-->
+    <link rel="shortcut icon" href="favicon.png">
+    <!-- Tweaks for older IEs--><!--[if lt IE 9]>
+        <script src="https://oss.maxcdn.com/html5shiv/3.7.3/html5shiv.min.js"></script>
+        <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script><![endif]-->
+  </head>
+  <body>
+    <!-- navbar-->
+    <header class="header mb-5">
+      <!--
+      *** TOPBAR ***
+      _________________________________________________________
+      --></div>
+      <nav class="navbar navbar-expand-lg">
+        <div class="container"><a href="index.php" class="navbar-brand home"><img src="img/logoapple.png" height="70" width="70" alt="Obaju logo" class="d-none d-md-inline-block"><img src="img/logo-small.png" alt="Obaju logo" class="d-inline-block d-md-none"><span class="sr-only">Apple Renta Kupang</span></a>
+          <div class="navbar-buttons">
+            <button type="button" data-toggle="collapse" data-target="#navigation" class="btn btn-outline-secondary navbar-toggler"><span class="sr-only">Toggle navigation</span><i class="fa fa-align-justify"></i></button>
+            <button type="button" data-toggle="collapse" data-target="#search" class="btn btn-outline-secondary navbar-toggler"><span class="sr-only">Toggle search</span><i class="fa fa-search"></i></button><a href="basket.php" class="btn btn-outline-secondary navbar-toggler"><i class="fa fa-shopping-cart"></i></a>
+          </div>
+          <div id="navigation" class="collapse navbar-collapse">
+              <ul class="navbar-nav mr-auto">
+              <li class="nav-item"><a href="detail.php" class="nav-link ">Produk</a></li>
+              <li class="nav-item dropdown menu-large"><a href="#" data-toggle="dropdown" data-hover="dropdown" data-delay="200" class="dropdown-toggle nav-link">Kategori<b class="caret"></b></a>
+                <ul class="dropdown-menu megamenu">
+                  <li>
+                    <div class="row">
+                      <div class="col-md-6 col-lg-3">
+                        <h5>Mainan Anak</h5>
+                        <ul class="list-unstyled mb-3">
+                          <li class="nav-item"><a href="index.php" class="nav-link">Mobil Aki</a></li>
+                          <li class="nav-item"><a href="category.php" class="nav-link">Playground</a></li>
+                          <li class="nav-item"><a href="category-right.php" class="nav-link">Playhouse</a></li>
+                          <li class="nav-item"><a href="category-full.php" class="nav-link">Motor Aki</a></li>
+                          <li class="nav-item"><a href="detail.php" class="nav-link"></a></li>
+                        </ul>
+                      </div>
+                      <div class="col-md-6 col-lg-3">
+                        <h5>Perlengkapan Anak</h5>
+                        <ul class="list-unstyled mb-3">
+                          <li class="nav-item"><a href="register.php" class="nav-link">Box Bayi</a></li>
+                          <li class="nav-item"><a href="customer-orders.php" class="nav-link">Stroller</a></li>
+                          <li class="nav-item"><a href="customer-order.php" class="nav-link">Baby Walker</a></li>
+                          <li class="nav-item"><a href="customer-wishlist.php" class="nav-link"></a></li>
+                          <li class="nav-item"><a href="customer-account.php" class="nav-link"></a></li>
+                        </ul>
+                      </div>
+                      
+                    </div>
+                  </li>
+                </ul>
+              </li>
+            </ul>
+            <div class="navbar-buttons d-flex justify-content-end">
+              <!-- /.nav-collapse-->
+				
+				<div id="search-not-mobile" class="navbar-collapse collapse"></div><a data-toggle="collapse" href="#search" class="btn navbar-btn btn-primary d-none d-lg-inline-block"><span class="sr-only">Toggle search</span><i class="fa fa-search"></i></a>
+				<div id="basket-overview" class="navbar-collapse collapse d-none d-lg-block"><a href="basket.php" class="btn btn-primary navbar-btn"><i class="fa fa-shopping-cart"></i><span>3 items in cart</span></a></div>
+				<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#login-modal">
+				  <i class="fa fa-user"></i>
+				</button>
+			</div>
+          </div>
+        </div>
+      </nav>
+      <div id="search" class="collapse">
+        <div class="container">
+          <form role="search" class="ml-auto">
+            <div class="input-group">
+              <input type="text" placeholder="Search" class="form-control">
+              <div class="input-group-append">
+                <button type="button" class="btn btn-primary"><i class="fa fa-search"></i></button>
+              </div>
+            </div>
+          </form>
+        </div>
+      </div>
+    </header>
+    <div id="all">
+      <div id="content">
+        <div class="container">
+          <div class="row">
+            <div class="col-md-12">
+              <div id="main-slider" class="owl-carousel owl-theme">
+                <div class="item"><img src="img/main-slider1.jpg" alt="" class="img-fluid"></div>
+                <div class="item"><img src="img/main-slider2.jpg" alt="" class="img-fluid"></div>
+                <div class="item"><img src="img/main-slider3.jpg" alt="" class="img-fluid"></div>
+                <div class="item"><img src="img/main-slider4.jpg" alt="" class="img-fluid"></div>
+              </div>
+              <!-- /#main-slider-->
+            </div>
+          </div>
+        </div>
+        <!--
+       *** ADVANTAGES HOMEPAGE ***
+        _________________________________________________________
+        -->
+        <div id="advantages">
+          <div class="container">
+            <div class="row mb-4">
+              <div class="col-md-4">
+                <div class="box clickable d-flex flex-column justify-content-center mb-0 h-100">
+                  <div class="icon"><i class="fa fa-heart"></i></div>
+                  <h3><a href="#">We love our customers</a></h3>
+                  <p class="mb-0">We are known to provide best possible service ever</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="box clickable d-flex flex-column justify-content-center mb-0 h-100">
+                  <div class="icon"><i class="fa fa-tags"></i></div>
+                  <h3><a href="#">Best prices</a></h3>
+                  <p class="mb-0">You can check that the height of the boxes adjust when longer text like this one is used in one of them.</p>
+                </div>
+              </div>
+              <div class="col-md-4">
+                <div class="box clickable d-flex flex-column justify-content-center mb-0 h-100">
+                  <div class="icon"><i class="fa fa-thumbs-up"></i></div>
+                  <h3><a href="#">100% satisfaction guaranteed</a></h3>
+                  <p class="mb-0">Free returns on everything for 3 months.</p>
+                </div>
+              </div>
+            </div>
+            <!-- /.row-->
+          </div>
+          <!-- /.container-->
+        </div>
+        <!-- /#advantages-->
+        <!-- *** ADVANTAGES END ***-->
+        <!--
+        *** HOT PRODUCT SLIDESHOW ***
+        _________________________________________________________
+        -->
+			<div id="hot">	
+				  <section class="bg-dark py-4 my-5">
+						<div class="container">
+							<div class="row">
+								<div class="col-12">
+									<h2 class="mb-3 text-danger">Produk</h2>
+								</div>
+									<?php
+									while($run=mysqli_fetch_array($data))
+									{
+										$nama=$run['nm_produk'];
+										?>
+								<div class="col-md-3 col-lg-4">
+								
+									<div class="card my-3">
+										<div class="card-thumbnail">
+											<img src="../uploads/<?php echo $run['foto']; ?>" class="img-fluid" alt="thumbnail">
+										</div>
+										<div class="card-body">
+										   <div class="text">
+											<h3><a href="detail.php?id=<?php echo $run['id_produk']; ?>"><?php echo $nama; ?></a></h3>
+											<!-- Group of default radios - option 1 -->
+											<div class="custom-control custom-radio">
+											  <input type="radio" class="custom-control-input" id="defaultGroupExample1" name="groupOfDefaultRadios">
+											  <label class="custom-control-label" for="defaultGroupExample1">2 Minggu <?php  echo' Rp.'. $run['hrg_mngg'];  ?></label>
+											</div>
+
+											<!-- Group of default radios - option 2 -->
+											<div class="custom-control custom-radio">
+											  <input type="radio" class="custom-control-input" id="defaultGroupExample2" name="groupOfDefaultRadios" checked>
+											  <label class="custom-control-label" for="defaultGroupExample2">4 Minggu/<?php  echo' Rp.'. $run['hrg_bln'];  ?></label>
+											</div>
+											<div class="d-grid px py-2">
+												<a class="btn btn-dark" href="#" role="button"><i class="fa fa-shopping-cart"></i></a>
+											</div>
+										  </div>
+										</div>
+									</div>
+								</div>
+							</div>
+						</div>
+					</section>
+<?php   }   ?>
+              <!-- /.product-slider-->
+            </div>
+            <!-- /.container-->
+          </div>
+          <!-- /#hot-->
+          <!-- *** HOT END ***-->
+        </div>
+        <!--
+        
+    *** FOOTER ***
+    _________________________________________________________
+    -->
+    <div id="footer">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">Pages</h4>
+            <ul class="list-unstyled">
+              <li><a href="text.php">About us</a></li>
+              <li><a href="text.php">Terms and conditions</a></li>
+              <li><a href="faq.php">FAQ</a></li>
+              <li><a href="contact.php">Contact us</a></li>
+            </ul>
+            <hr>
+            <h4 class="mb-3">User section</h4>
+            <ul class="list-unstyled">
+              <li><a href="#" data-toggle="modal" data-target="#login-modal">Login</a></li>
+              <li><a href="register.php">Regiter</a></li>
+            </ul>
+          </div>
+          <!-- /.col-lg-3-->
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">Top categories</h4>
+            <h5>Men</h5>
+            <ul class="list-unstyled">
+              <li><a href="category.php">T-shirts</a></li>
+              <li><a href="category.php">Shirts</a></li>
+              <li><a href="category.php">Accessories</a></li>
+            </ul>
+            <h5>Ladies</h5>
+            <ul class="list-unstyled">
+              <li><a href="category.php">T-shirts</a></li>
+              <li><a href="category.php">Skirts</a></li>
+              <li><a href="category.php">Pants</a></li>
+              <li><a href="category.php">Accessories</a></li>
+            </ul>
+          </div>
+          <!-- /.col-lg-3-->
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">Where to find us</h4>
+            <p><strong>Obaju Ltd.</strong><br>13/25 New Avenue<br>New Heaven<br>45Y 73J<br>England<br><strong>Great Britain</strong></p><a href="contact.php">Go to contact page</a>
+            <hr class="d-block d-md-none">
+          </div>
+          <!-- /.col-lg-3-->
+          <div class="col-lg-3 col-md-6">
+            <h4 class="mb-3">Get the news</h4>
+            <p class="text-muted">Pellentesque habitant morbi tristique senectus et netus et malesuada fames ac turpis egestas.</p>
+            <form>
+              <div class="input-group">
+                <input type="text" class="form-control"><span class="input-group-append">
+                  <button type="button" class="btn btn-outline-secondary">Subscribe!</button></span>
+              </div>
+              <!-- /input-group-->
+            </form>
+            <hr>
+            <h4 class="mb-3">Stay in touch</h4>
+            <p class="social"><a href="#" class="facebook external"><i class="fa fa-facebook"></i></a><a href="#" class="twitter external"><i class="fa fa-twitter"></i></a><a href="#" class="instagram external"><i class="fa fa-instagram"></i></a><a href="#" class="gplus external"><i class="fa fa-google-plus"></i></a><a href="#" class="email external"><i class="fa fa-envelope"></i></a></p>
+          </div>
+          <!-- /.col-lg-3-->
+        </div>
+        <!-- /.row-->
+      </div>
+      <!-- /.container-->
+    </div>
+    <!-- /#footer-->
+    <!-- *** FOOTER END ***-->
+    
+    
+    <!--
+    *** COPYRIGHT ***
+    _________________________________________________________
+    -->
+    <div id="copyright">
+      <div class="container">
+        <div class="row">
+          <div class="col-lg-6 mb-2 mb-lg-0">
+            <p class="text-center text-lg-left">©2019 Your name goes here.</p>
+          </div>
+          <div class="col-lg-6">
+            <p class="text-center text-lg-right">Template design by <a href="https://bootstrapious.com/">Bootstrapious</a>
+              <!-- If you want to remove this backlink, pls purchase an Attribution-free License @ https://bootstrapious.com/p/obaju-e-commerce-template. Big thanks!-->
+            </p>
+          </div>
+        </div>
+      </div>
+    </div>
+    <!-- *** COPYRIGHT END ***-->
+    <!-- JavaScript files-->
+    <script src="vendor/jquery/jquery.min.js"></script>
+    <script src="vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="vendor/jquery.cookie/jquery.cookie.js"> </script>
+    <script src="vendor/owl.carousel/owl.carousel.min.js"></script>
+    <script src="vendor/owl.carousel2.thumbs/owl.carousel2.thumbs.js"></script>
+    <script src="js/front.js"></script>
+  </body>
+</html>
